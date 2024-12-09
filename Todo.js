aafe942f8c10efee99f31825e6201c81099f5c6f
anujsaini9557 <aaasainianuj@gmail.com>
2024-12-09 22:02:49 +0530

    initial todo app

---
+++ Todo.js
@@ -0,0 +1,51 @@
+console.log("jai shree ram");
+
+let todoList = [
+  {
+    item: "Buy milk",
+    dueDate: '04-10-23'
+  },
+
+  {
+    item: "Go To College",
+    dueDate: '04-10-23'
+  },
+];
+
+displayItems();
+
+function addTodo() {
+  let inputElement = document.querySelector('#todo-input');
+  let dateElement = document.querySelector('#todo-date');
+
+  let todoItem = inputElement.value;
+  let todoDate = dateElement.value;
+  todoList.push({ item: todoItem, dueDate: todoDate });
+  inputElement.value = '';
+  dateElement.value = '';
+
+  displayItems();
+}
+
+function displayItems() {
+  let containerElement = document.querySelector('.todo-container');
+
+  let newHtml = '';
+
+  for (let i = 0; i < todoList.length; i++) {
+    // let item = todoList[i].item;
+    // let dueDate = todoList[i].dueDate;
+
+     let {item, dueDate } = todoList[i];
+    newHtml += `
+  
+   <span>${item}</span>
+   <span>${dueDate}</span>
+   
+    <button class="delete" onclick="todoList.splice(${i},1);
+    displayItems();"> Delete </button>
+    
+   `;
+  }
+  containerElement.innerHTML = newHtml;
+}
